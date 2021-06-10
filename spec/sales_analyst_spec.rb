1cd49d70ae2a186b3ec2b6eefcc295c92b4d0018
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-09 18:00:45 -0600

    Create tests iteration 4 methods

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -149,6 +149,12 @@ RSpec.describe 'SalesAnalyst' do
       expect(@sa.total_revenue_by_date("2012-03-27")).to eq(254792.59)
     end
 
+    it 'can determine revenue by invoice id' do
+      expect(@sa.revenue_by_invoice_id(5)).to eq(4912.1)
+    end
 
+    it 'can return revenue by merchant' do
+      expect(@sa.revenue_by_merchant(12334123)).to eq(58859.75)
+    end
   end
 end
